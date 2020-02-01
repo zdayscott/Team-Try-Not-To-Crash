90efb5d7fb499e671127a3d920769a56775920bb
Bonzu <jeffreyrussellgm@gmail.com>
2020-02-01 14:13:54 -0800

    Push asteroids

---
+++ Assets/Scripts/Jeff/AstroidMover.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroidMover : MonoBehaviour
+{
+    private GameObject rock;
+    private float xInitForce = 1f;
+    private float yInitForce = 1f;
+    private float maxRotation;
+    private float rotZ;
+    private Rigidbody2D rb;
+    private int _generation = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rock = this.gameObject;
+        rb = rock.GetComponent<Rigidbody2D>();
+        maxRotation = 25f;
+        rotZ = Random.Range(-maxRotation, maxRotation);
+
+        float speedY = Random.Range(200f, 800f);
+        int selectorY = Random.Range(0, 2);
+
+        float dirY = 0;
+        if (selectorY == 1)
+        {
+            dirY = -1;
+        }
+        else
+        {
+            dirY = 1;
+        }
+        float finalSpeedY = speedY * dirY;
+        float xSpeed = Random.Range(-100, 100);
+        float ySpeed = Random.Range(-100, 100);
+        rb.AddForce(new Vector3(1f * xSpeed, 1f * ySpeed, 0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rock.transform.Rotate(new Vector3(0f, 0f, rotZ) * Time.deltaTime);
+        //checkPosition();
+        float dynamicMaxSpeed = 3f;
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -dynamicMaxSpeed, dynamicMaxSpeed), Mathf.Clamp(rb.velocity.y, -dynamicMaxSpeed, dynamicMaxSpeed));
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+       if (collision.tag == "Boarder")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Jeff/AstroidSpawner.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroidSpawner : MonoBehaviour
+{
+    public int chance;
+    public GameObject rock;
+    private Rigidbody2D rb;
+    private Vector3 playerPos;
+    public Transform player;
+    public float minDistance = 10;
+    
+    private float nextAst;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPos = player.position;
+        nextAst = Random.Range(0, 2);
+        CreateSmallAsteroids();
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        nextAst -= Time.deltaTime;
+        if(nextAst < Time.deltaTime)
+        {
+            CreateSmallAsteroids();
+            nextAst = Random.Range(1, 4);
+        }
+
+    }
+
+    float SpawnDistance(Vector3 playerPos, Vector3 spawnPos)
+    {
+        float distance;
+        distance = Mathf.Sqrt(Mathf.Pow((playerPos.x - spawnPos.x), 2) + Mathf.Pow((playerPos.y - spawnPos.y), 2) + Mathf.Pow((playerPos.z - spawnPos.z), 2));
+        return distance;
+    }
+    void CreateSmallAsteroids()
+    {
+        rb = rock.GetComponent<Rigidbody2D>();
+      //  float distance;
+        float xPos;
+        float zPos;
+        Vector3 rockPos;
+        //this checks if asteroids is going to spawn too close to the player
+       // do {
+            xPos = Random.Range(-10, 10);
+            zPos = Random.Range(-10, 10);
+            rockPos = new Vector3(xPos, 0, zPos);            
+         //   distance = SpawnDistance(playerPos, rockPos);
+       // } while (distance < minDistance);
+
+       // print("Rock spawned at " + xPos + ", 0, " + zPos);
+        GameObject AsteroidClone = Instantiate(rock, rockPos, Quaternion.identity);
+        float xSpeed;
+        float ySpeed;
+        if (xPos > 0)
+        {
+            xSpeed = Random.Range(-100, 0);
+        }
+        else
+        {
+            xSpeed = Random.Range(0, 100);
+        }
+
+       
+        
+         Random.Range(-100, 100);
+        rb.AddForce(new Vector3(xSpeed, 155, 0f));
+    }
+}
