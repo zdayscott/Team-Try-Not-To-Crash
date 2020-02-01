adb0711ae242f608dca8c53c161a73f300561ce0
Dane Carstens <dcarsten@uci.edu>
2020-01-31 23:11:15 -0800

    help?

---
+++ EIN is Sad/Assets/Scripts/EINmanager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class EINmanager : MonoBehaviour
+{
+    public TextAsset firstWorld;
+
+
+    private List<string> firstWorldLines;
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstWorldLines = firstWorld.text.Split('\n').ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void firstButton()
+    {
+        print("first button clicked");
+    }
+}
