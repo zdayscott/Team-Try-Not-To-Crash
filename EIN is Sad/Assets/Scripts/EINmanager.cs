0f635403e18db902554c0a21f445f895624e1165
Dane Carstens <dcarsten@uci.edu>
2020-02-01 10:43:55 -0800

    made prototype for EIN gameplay

--- EIN is Sad/Assets/Scripts/EINmanager.cs
+++ EIN is Sad/Assets/Scripts/EINmanager.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.UI;
+using System;
 
 public class EINmanager : MonoBehaviour
 {
     public TextAsset firstWorld;
+    public Text displayText;
 
 
     private List<string> firstWorldLines;
@@ -13,6 +16,8 @@ public class EINmanager : MonoBehaviour
     void Start()
     {
         firstWorldLines = firstWorld.text.Split('\n').ToList();
+        displayText.text = "Hello world. My name is E.I.N., or Emotionally Intelligent Network. How can I assist you today?";
+        print(getLine("insert line here|1, 2"));
     }
 
     // Update is called once per frame
@@ -20,9 +25,32 @@ public class EINmanager : MonoBehaviour
     {
         
     }
+    
+    private string getLine(string totalLine)
+    {
+        string line = totalLine.Split('|')[0];
+        return line;
+    }
+
+    private List<int> getInts(string totalLine)
+    {
+        string line = totalLine.Split('|')[1];
+        List<string> intStrings = line.Split(',').ToList();
+        List<int> ints = new List<int>();
+        for (int i = 0; i < intStrings.Count; ++i)
+        {
+            ints.Add(Int32.Parse(intStrings[i]));
+        }
+        return ints;
+    }
 
     public void firstButton()
     {
         print("first button clicked");
+    }
+
+    public void secondButton()
+    {
+        print("second button clicked");
     }
 }
