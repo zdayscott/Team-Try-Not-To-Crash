e33a8be286c58dce86123cc477dc29deaa4057f7
Dane Carstens <dcarsten@uci.edu>
2020-02-01 14:46:34 -0800

    scene changing work

---
+++ Assets/Scripts/Dane/sceneManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+    string currentScene;
+    string creditsScene = "creditsScene";
+    string titleScene = "titleScreen";
+    private void Awake()
+    {
+        currentScene = SceneManager.GetActiveScene().name;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("escape") && currentScene == creditsScene)
+        {
+            switchToTitleScene();
+        }
+    }
+    public void switchToCredits()
+    {
+        SceneManager.LoadScene(creditsScene);
+    }
+
+    public void switchToTitleScene()
+    {
+        SceneManager.LoadScene(titleScene);
+    }
+}
