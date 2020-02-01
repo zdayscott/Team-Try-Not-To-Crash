cba417a95299060eb5d35a9b36228f30f30521b2
zdayscott <zdayscott@gmail.com>
2020-01-31 22:14:44 -0800

    Push With folders and Ship

--- EIN is Sad/Assets/Scripts/Zach/ShipController.cs
+++ EIN is Sad/Assets/Scripts/Zach/ShipController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
+
+    private IThruster mThruster;
+    private ITurner mTurner;
+    private IShooter mShooter;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +17,34 @@ public class ShipController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if(Input.GetButtonDown("Vertical"))
+        {
+
+        }
+    }
+}
+
+public interface IThruster
+{
+    public void Thrust()
+    {
+
+    }
+}
+
+public interface ITurner
+{
+    public void Turn()
+    {
+
+    }
+
+}
+
+public interface IShooter
+{
+        public void Shoot()
     {
         
     }
