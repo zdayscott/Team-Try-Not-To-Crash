c40117766375843c05b43fd64537c9c64c309e8d
Dane Carstens <dcarsten@uci.edu>
2020-02-02 11:15:20 -0800

    Added end scene, fixed one string issue

--- Assets/Scripts/Dane/sceneManager.cs
+++ Assets/Scripts/Dane/sceneManager.cs
@@ -9,6 +9,12 @@ public class sceneManager : MonoBehaviour
     string creditsScene = "creditsScene";
     string titleScene = "titleScreen";
     string mainGame = "mainGame";
+    string endScene = "endGame";
+
+    public float percentCorrect = 0f;
+    public int asteroidsHit = 0;
+    public float timeSurvived = 0f;
+
     private void Awake()
     {
         currentScene = SceneManager.GetActiveScene().name;
@@ -44,11 +50,14 @@ public class sceneManager : MonoBehaviour
     }
     public void switchToGame()
     {
-
-       
         SceneManager.LoadScene(mainGame);
         SoundManager.instance.EinIsHappySfx();
         SoundManager.instance.EinPulseStart();
         SoundManager.instance.MusicStop();
+    }
+
+    public void endGame()
+    {
+        SceneManager.LoadScene(endScene);
     }
 }
