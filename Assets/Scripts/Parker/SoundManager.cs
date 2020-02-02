a630e8b9021d2dbdcbb952e06c43dc25aef1f0ab
Parker Duncan <Duncan.parker.h@gmail.com>
2020-02-02 05:53:34 -0800

    Basic sound manager

---
+++ Assets/Scripts/Parker/SoundManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource shipSfxSource;
+    public AudioSource einSfxSource;
+
+    public static SoundManager instance = null;
+
+    public float lowPitchRando = .90f;
+    public float highPitchRando = 1.05f;
+    // Start is called before the first frame update
+    void Awake()
+   
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlaySfx (AudioClip clip)
+    {
+        shipSfxSource.clip = clip;
+        shipSfxSource.Play ();
+
+
+    }
+
+    public void PlayRandomSfx (params AudioClip [] clips)
+    {
+        int randomIndex = Random.Range(0, clips.Length);
+        float randoPitch = Random.Range(lowPitchRando, highPitchRando);
+
+        shipSfxSource.pitch = randoPitch;
+        shipSfxSource.clip = clips[randomIndex];
+        shipSfxSource.Play();
+
+
+
+    }
+
+
+}
