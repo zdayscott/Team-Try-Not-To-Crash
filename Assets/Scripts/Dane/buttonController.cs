8c4566d38affd3b64228793d2e12f8aff1f27769
Dane Carstens <dcarsten@uci.edu>
2020-02-02 12:26:53 -0800

    Buttons work and now scene manager is singleton

---
+++ Assets/Scripts/Dane/buttonController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonController : MonoBehaviour
+{
+    private Button playGame;
+    private Button credits;
+    private Button quitGame;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playGame = GameObject.Find("playGame").GetComponent<Button>();
+        credits = GameObject.Find("credits").GetComponent<Button>();
+        quitGame = GameObject.Find("quitGame").GetComponent<Button>();
+
+        if (playGame != null)
+        {
+            playGame.onClick.AddListener(sceneManager.Instance.switchToGame);
+        }
+        if (credits != null)
+        {
+            credits.onClick.AddListener(sceneManager.Instance.switchToCredits);
+        }
+        if (credits != null)
+        {
+            quitGame.onClick.AddListener(sceneManager.Instance.quitGame);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Dane/sceneManager.cs
+++ Assets/Scripts/Dane/sceneManager.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class sceneManager : MonoBehaviour
 {
+    private static sceneManager _instance;
+
+    public static sceneManager Instance { get {return _instance; }}
+
+    public bool manualSwitch = false;
+
     string currentScene;
     string creditsScene = "creditsScene";
     string titleScene = "titleScreen";
@@ -15,10 +22,56 @@ public class sceneManager : MonoBehaviour
     public int asteroidsHit = 0;
     public float timeSurvived = 0f;
 
+    private Button playGame;
+    private Button credits;
+    private Button quitButton;
+    private Button returnToTitle;
+
+    private float timeElapsed = 0;
+
     private void Awake()
     {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
         currentScene = SceneManager.GetActiveScene().name;
         DontDestroyOnLoad(this.gameObject);
+    }
+    private void buttonAssigner()
+    {
+        if (GameObject.Find("playGame") != null)
+        {
+            print("found play game");
+            playGame = GameObject.Find("playGame").GetComponent<Button>();
+            playGame.onClick.AddListener(switchToGame);
+        }
+        if (GameObject.Find("credits") != null)
+        {
+            print("found credits");
+            credits = GameObject.Find("credits").GetComponent<Button>();
+            credits.onClick.AddListener(switchToCredits);
+        }
+        if (GameObject.Find("quitGame") != null)
+        {
+            print("quit game found");
+            quitButton = GameObject.Find("quitGame").GetComponent<Button>();
+            quitButton.onClick.AddListener(quitGame);
+        }
+        if (GameObject.Find("returnToTitle") != null)
+        {
+            print("return to title found");
+            returnToTitle = GameObject.Find("returnToTitle").GetComponent<Button>();
+            returnToTitle.onClick.AddListener(switchToTitleScene);
+        }
+    }
+    private void Start()
+    {
+        buttonAssigner();
     }
 
     private void Update()
@@ -27,37 +80,53 @@ public class sceneManager : MonoBehaviour
         {
             switchToTitleScene();
         }
+
+        if (manualSwitch && Input.GetKeyDown("p"))
+        {
+            endGame();
+        }
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed >= 1f)
+        {
+            buttonAssigner();
+            timeElapsed = 0f;
+        }
     }
     public void switchToCredits()
     {
-       
         SceneManager.LoadScene(creditsScene);
+        buttonAssigner();
     }
 
     public void switchToTitleScene()
     {
-
-
         SceneManager.LoadScene(titleScene);
         SoundManager.instance.MusicStart();
         SoundManager.instance.EinIsDead();
         SoundManager.instance.EinPulseStop();
+        buttonAssigner();
 
     }
     public void quitGame()
     {
+        //buttonAssigner();
+        print("trying to quit");
         Application.Quit();
     }
+
     public void switchToGame()
     {
         SceneManager.LoadScene(mainGame);
         SoundManager.instance.EinIsHappySfx();
         SoundManager.instance.EinPulseStart();
         SoundManager.instance.MusicStop();
+        buttonAssigner();
     }
 
     public void endGame()
     {
         SceneManager.LoadScene(endScene);
+        buttonAssigner();
+
     }
 }
